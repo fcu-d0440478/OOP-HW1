82cfa0cbba797268bb84700853ae2b684a03f0a8
user <user@FG08>
2016-09-30 09:31:27 +0800

    version a=10

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,9 +3,9 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		
+		int a=10;
 		System.out.println("Hello OOP");
-	
+		System.out.println("a="+a);
 	}
 
 }
