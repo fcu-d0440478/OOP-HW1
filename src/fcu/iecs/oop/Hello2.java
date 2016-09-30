3de48ac0af3542a088575482b2f22d16afad039e
user <user@FG08>
2016-09-30 09:48:38 +0800

    Origin lyrics

---
+++ src/fcu/iecs/oop/Hello2.java
@@ -0,0 +1,10 @@
+package fcu.iecs.oop;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+		System.out.println(lyrics);
+	}
+
+}
